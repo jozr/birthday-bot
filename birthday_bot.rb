7bb4873233ff701cb670964f371d0f42b4edda73
Josephine Wright <jozwright@gmail.com>
2017-10-20 14:39:06 +0200

    Add token

--- birthday_bot.rb
+++ birthday_bot.rb
@@ -1,7 +1,7 @@
 require 'slack-ruby-bot'
 
 class BirthdayBot < SlackRubyBot::Bot
-  match /^birthday$/ do |client, data, match|
+  command /^birthday$/ do |client, data, match|
     client.say(channel: data.channel, text: "When is your birhday?")
   end
 end
